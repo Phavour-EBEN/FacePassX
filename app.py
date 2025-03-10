4bf6f30e37956b239fa6a7131700e9041c4a5fd1
Phavour-EBEN <ainooebenezer05@gmail.com>
2025-03-10 21:58:39 +0000

    added deployment files

--- app.py
+++ app.py
@@ -149,6 +149,9 @@ def verify_face():
             'error': error_msg
         })
 
+# At the end of your app.py file, change:
 if __name__ == '__main__':
-    # Run the Flask app
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # Get port from environment variable or default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+   
